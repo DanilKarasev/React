ca9adff4e4c0ba571f5ca05bc4900ea7f3efc550
DanilKarasev <88264176+DanilKarasev@users.noreply.github.com>
2021-11-23 01:55:57 +1000

    Lesson 3 homework

--- src/Components/MessageList/MessageList.jsx
+++ src/Components/MessageList/MessageList.jsx
@@ -12,7 +12,8 @@ export const MessageList = () => {
         if (messageList.length) {
             if (messageList[messageList.length - 1].author === 'ME') {
                 inputRef.current = setTimeout(() => {
-                    setMessageList([...messageList, {message: faker.lorem.sentence(), author: faker.name.findName(), id:faker.datatype.uuid()}]);
+                    let newDate = new Date();
+                    setMessageList([...messageList, {message: faker.lorem.sentence(), author: faker.name.findName(), id:faker.datatype.uuid(), time: newDate.toTimeString().split(' ')[0].slice(0, -3)}]);
                 }, 1500);
                 return () => clearInterval(inputRef.current);
             }
@@ -26,7 +27,8 @@ export const MessageList = () => {
 
     const addMessage = (e) => {
         e.preventDefault();
-        setMessageList([...messageList, {message, author: 'ME', id:faker.datatype.uuid()}]);
+        let newDate = new Date();
+        setMessageList([...messageList, {message, author: 'ME', id:faker.datatype.uuid(), time: newDate.toTimeString().split(' ')[0].slice(0, -3)}]);
         setMessage('')
     }
 
@@ -34,14 +36,21 @@ export const MessageList = () => {
 
     return (
         <div className={"Chat"}>
+            <div className="Chat-header">OutstandingChat</div>
             <div className={"Chat-body"}>
-                {messageList.map(({message, author, id}) => (
-                    <div key={id} className={(author === 'ME') ? "Message Message-me" : "Message"}><h4>{author}</h4> <p>{message}</p></div>
+                {messageList.map(({message, author, id, time}) => (
+                    <div key={id} className={(author === 'ME') ? "Message Message-me" : "Message"}>
+                        <h4>{author}</h4>
+                        <div className={"Message-box"}>
+                            <div className={"Message-text"}>{message}</div>
+                            <div className={"Message-time"}>{time}</div>
+                        </div>
+                    </div>
                 ))}
             </div>
             <div className={"Chat-footer"}>
                 <form action="submit" onSubmit={addMessage}>
-                    <input ref={inputRef} value={message} onChange={handleMessage} placeholder={"Message"} type="text" required/> <button type={"submit"}/>
+                    <input ref={inputRef} value={message} onChange={handleMessage} placeholder={"Message"} type="text" required autoFocus={true}/> <button type={"submit"}/>
                 </form>
             </div>
         </div>
