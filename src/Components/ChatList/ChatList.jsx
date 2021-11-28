86454c5b0f05e5bd3fd16cfc8875f410b4bcd1e5
DanilKarasev <88264176+DanilKarasev@users.noreply.github.com>
2021-11-28 20:25:46 +1000

    Added ChatAdding function

--- src/Components/ChatList/ChatList.jsx
+++ src/Components/ChatList/ChatList.jsx
@@ -1,5 +1,5 @@
 import ListSubheader from "@mui/material/ListSubheader";
-import { Link , useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { ROUTES } from "../../Router/constants";
 import ListItemButton from "@mui/material/ListItemButton";
 import Avatar from "@mui/material/Avatar";
@@ -7,26 +7,38 @@ import ListItemText from "@mui/material/ListItemText";
 import List from "@mui/material/List";
 import React from "react";
 
+export const ChatList = ({ filteredChats }) => {
+  const location = useLocation();
 
-export const ChatList = ( { filteredChats } ) => {
-    const location = useLocation ();
+  let currentChatName = location.pathname.split("/");
+  currentChatName = currentChatName[currentChatName.length - 1];
 
-    let currentChatName = location.pathname.split ( '/' );
-    currentChatName = currentChatName[ currentChatName.length - 1 ]
-
-    return (
-        <List component="nav" aria-labelledby="nested-list-subheader"
-              subheader={
-                  <ListSubheader component="div" id="nested-list-subheader"> Список чатов </ListSubheader>
-              }>
-            { filteredChats.map ( ( { name , avatar } ) => (
-                <Link key={ name } to={ ROUTES.CHAT + name }>
-                    <ListItemButton selected={ name === currentChatName } sx={ { gap : '10px' } }>
-                        <Avatar alt={ name } src={ avatar }/>
-                        <ListItemText primary={ name }/>
-                    </ListItemButton>
-                </Link>
-            ) ) }
-        </List>
-    );
-}
+  return (
+    <div className={"Chat-content"}>
+      <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            {" "}
+            Список чатов{" "}
+          </ListSubheader>
+        }
+      >
+        {filteredChats.map(({ name, avatar }) => (
+          <Link key={name} to={ROUTES.CHAT + name}>
+            <ListItemButton
+              selected={name === currentChatName}
+              sx={{ gap: "10px" }}
+            >
+              <Avatar alt={name} src={avatar}>
+                {name[0]}
+              </Avatar>
+              <ListItemText primary={name} />
+            </ListItemButton>
+          </Link>
+        ))}
+      </List>
+    </div>
+  );
+};
