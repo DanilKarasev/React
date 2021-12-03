5dd5eae3b9c40f830805c802de45508d9dbfe378
DanilKarasev <88264176+DanilKarasev@users.noreply.github.com>
2021-12-03 16:41:37 +1000

    Lesson 5 homework. ez.

--- src/Screens/Profile/Profile.jsx
+++ src/Screens/Profile/Profile.jsx
@@ -1,13 +1,47 @@
-import React from "react";
-import "./Profile.sass"
+import "./Profile.sass";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleProfileInfoAction } from "../../Store/Profile/actions";
+import { profileSelector } from "./selectors";
+import { Switch } from "@mui/material";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 
 export const Profile = () => {
-    return (
-        <div className={"Profile"}>
-            <div className={"Profile-header"}>Profile name</div>
-            <div className={"Profile-info"}>
-                <div className={"Profile-info-content"}>Some profile info</div>
-            </div>
+  const dispatch = useDispatch();
+  const { profileInfo, showProfileInfo } = useSelector(profileSelector);
+
+  const handleToggleShowProfileInfo = () => {
+    dispatch(toggleProfileInfoAction());
+  };
+
+  return (
+    <div className={"Profile"}>
+      <div className={"Profile-header"}>Profile name</div>
+      <div className={"Profile-main"}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                color={"warning"}
+                checked={showProfileInfo}
+                onChange={handleToggleShowProfileInfo}
+              />
+            }
+            label="Show profile info"
+          />
+        </FormGroup>
+        <div className={"Profile-info"}>
+          <div
+            className={
+              showProfileInfo
+                ? "Profile-info-content"
+                : "Profile-info-content__hidden"
+            }
+          >
+            {showProfileInfo && profileInfo}
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
---
+++ src/Store/Profile/actions.js
@@ -0,0 +1,5 @@
+import { TOGGLE_PROFILE_INFO_ACTION } from "./constants";
+
+export const toggleProfileInfoAction = () => ({
+  type: TOGGLE_PROFILE_INFO_ACTION,
+});
