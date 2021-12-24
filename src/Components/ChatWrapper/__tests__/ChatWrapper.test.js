5d521f3e53e635db392288a484881f4381accf0d
DanilKarasev <88264176+DanilKarasev@users.noreply.github.com>
2021-12-25 00:11:39 +1000

    С тестами ниче не понял. Буду разбираться позже.

--- src/Components/ChatWrapper/__tests__/ChatWrapper.test.js
+++ src/Components/ChatWrapper/__tests__/ChatWrapper.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { ChatWrapper } from "../ChatWrapper";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(<ChatWrapper />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
